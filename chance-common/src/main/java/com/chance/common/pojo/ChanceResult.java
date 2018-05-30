21a582718812e0c936d1a3cc76c4cc3c4ba9d69a
Chance <zzhchance@163.com>
2018-05-30 21:03:42 +0800

    Chance商城自定义响应结构封装类

---
+++ chance-common/src/main/java/com/chance/common/pojo/ChanceResult.java
@@ -0,0 +1,150 @@
+package com.chance.common.pojo;
+
+import java.util.List;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Chance商城自定义响应结构
+ */
+public class ChanceResult {
+
+    // 定义jackson对象
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    // 响应业务状态
+    private Integer status;
+
+    // 响应消息
+    private String msg;
+
+    // 响应中的数据
+    private Object data;
+
+    public static ChanceResult build(Integer status, String msg, Object data) {
+        return new ChanceResult(status, msg, data);
+    }
+
+    public static ChanceResult ok(Object data) {
+        return new ChanceResult(data);
+    }
+
+    public static ChanceResult ok() {
+        return new ChanceResult(null);
+    }
+
+    public ChanceResult() {
+
+    }
+
+    public static ChanceResult build(Integer status, String msg) {
+        return new ChanceResult(status, msg, null);
+    }
+
+    public ChanceResult(Integer status, String msg, Object data) {
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public ChanceResult(Object data) {
+        this.status = 200;
+        this.msg = "OK";
+        this.data = data;
+    }
+
+//    public Boolean isOK() {
+//        return this.status == 200;
+//    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    /**
+     * 将json结果集转化为TaotaoResult对象
+     * 
+     * @param jsonData json数据
+     * @param clazz TaotaoResult中的object类型
+     * @return
+     */
+    public static ChanceResult formatToPojo(String jsonData, Class<?> clazz) {
+        try {
+            if (clazz == null) {
+                return MAPPER.readValue(jsonData, ChanceResult.class);
+            }
+            JsonNode jsonNode = MAPPER.readTree(jsonData);
+            JsonNode data = jsonNode.get("data");
+            Object obj = null;
+            if (clazz != null) {
+                if (data.isObject()) {
+                    obj = MAPPER.readValue(data.traverse(), clazz);
+                } else if (data.isTextual()) {
+                    obj = MAPPER.readValue(data.asText(), clazz);
+                }
+            }
+            return build(jsonNode.get("status").intValue(), jsonNode.get("msg").asText(), obj);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * 没有object对象的转化
+     * 
+     * @param json
+     * @return
+     */
+    public static ChanceResult format(String json) {
+        try {
+            return MAPPER.readValue(json, ChanceResult.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Object是集合转化
+     * 
+     * @param jsonData json数据
+     * @param clazz 集合中的类型
+     * @return
+     */
+    public static ChanceResult formatToList(String jsonData, Class<?> clazz) {
+        try {
+            JsonNode jsonNode = MAPPER.readTree(jsonData);
+            JsonNode data = jsonNode.get("data");
+            Object obj = null;
+            if (data.isArray() && data.size() > 0) {
+                obj = MAPPER.readValue(data.traverse(),
+                        MAPPER.getTypeFactory().constructCollectionType(List.class, clazz));
+            }
+            return build(jsonNode.get("status").intValue(), jsonNode.get("msg").asText(), obj);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+}
